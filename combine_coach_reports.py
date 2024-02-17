b8cde4e345213a81ed16b1503e2f7ba663295c85
Odyhibit <josh.2990@gmail.com>
2024-02-17 12:01:22 -0600

    cleanup code for spring 2024.

--- combine_coach_reports.py
+++
@@ -1,3 +0,0 @@
-# open each file in the coach reports directory
-    # find the category with smallest levenstein distance
-        # if that category has already been used, throw a name error that is clear, and exit.
